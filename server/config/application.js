925073cb8475c42f9ee5c0188459f487d2c43d3a
raj-sampath <raj_sampath@msn.com>
2018-01-12 19:50:50 +0530

    Removing the credentials

--- server/config/application.js
+++ server/config/application.js
@@ -1,17 +1,16 @@
-var bucketName = "roonga-roonga-roonga";
+var bucketName = "";
 
 module.exports = {
-    _SMTP_HOSTNAME: "email-smtp.us-west-2.amazonaws.com",
+    _SMTP_HOSTNAME: "",
     _SMTP_PORT: 465,
-    _SMTP_USERNAME: "AKIAJ2BLJ6OJQA2XWI4Q",
-    _SMTP_PASSWORD: "Am9lLeORG7zp74ePSgnWGgbOWiYOmwxboqS2vnOK5xuM",
-    _FROM_ADDRESS: "shyamraj.sampath@agilizTech.com",
+    _SMTP_USERNAME: "",
+    _SMTP_PASSWORD: "",
+    _FROM_ADDRESS: "s",
     _AWS_S3_BUCKET_NAME: bucketName,
     _AWS_S3_API_VERSION: "2006-03-01",
     __AWS_S3_IMAGE_URL: () => {
         return "https://s3-us-west-2.amazonaws.com/" + bucketName + "/";
     },
-    _STRIPE_PUBLISHING_KEY: "pk_test_CvqigMWVYEVSgfSc7q0DVDrC",
-    _STRIPE_SECRET_KEY: "sk_test_GxzH7IK52Zio59tT5R248AWP",
-    _PERCENTAGE_ROUND_PRECISION: 2
+    _STRIPE_PUBLISHING_KEY: "",
+    _STRIPE_SECRET_KEY: ""
 }
